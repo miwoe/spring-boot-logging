48e77a1561839edd0094648b8c0b6bc11af0a0e1
Michael Woelm <mail@m-woelm.de>
2016-07-13 21:57:55 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,20 @@
+Practical logging in Spring Boot
+---------------------------------
+
+This setup shows how to configure logging for use in a production ready environment.
+
+Making some assumptions:
+
+- You want a common logging configuration for ALL environments:
+
+  - Same appenders
+  - Different logging levels
+  - Same patterns
+
+
+Externalizing configuration via Springs default configuration framework (or maybe advanced cloud concepts...)
+
+This means:
+- Basic setup in code
+- Environment specific setup in application.properties (or other possible config file)
+
---
+++ src/main/java/de/miwoe/MyController.java
@@ -0,0 +1,24 @@
+package de.miwoe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Grauschleier on 13.07.2016.
+ */
+@Controller
+public class MyController {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(MyController.class);
+
+    @RequestMapping(value = "/foo")
+    public @ResponseBody String test() {
+        logger.warn("foo {}", 1);
+        return "foo";
+    }
+
+}
